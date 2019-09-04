2a2e1db4bca2a1948a1172065273b4f3a9636394
Andrew Tang <atreeleaf@gmail.com>
2019-09-03 20:58:15 -0700

    added mock image middle

--- client/components/Body.tsx
+++ client/components/Body.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Body() {
   return (
     <div className='main-body'>
+      <img src = 'https://raw.githubusercontent.com/rohan-varma/rohan-blog/gh-pages/images/graphql.png' id = 'graph-ql'></img>
       <h3 id = 'main-description'>Goblin Monitor. Monitor your GraphQL endpoint.</h3>
       <div id = 'action-buttons-wrapper'>
         <button className = 'action-btns'> Get Started </button>
