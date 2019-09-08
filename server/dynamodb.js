8a4a4f9808d965877cb7625e29f99945aba2a433
Andrew Tang <atreeleaf@gmail.com>
2019-09-08 15:12:07 -0700

    parameters for all types of queries are now complete

--- server/dynamodb.js
+++ server/dynamodb.js
@@ -62,10 +62,10 @@ lambda.invoke(lambdaParams,(err,data) => {
 */
 
 
-db.get(params, (err,data)=>{
-  if (err) console.log(err,err.stack);
-  // else console.log(data)
-})
+// db.get(params, (err,data)=>{
+//   if (err) console.log(err,err.stack);
+//   // else console.log(data)
+// })
 
 
 
@@ -73,9 +73,6 @@ db.get(params, (err,data)=>{
 // queryField = main object key 
 // fieldName = keys within queryField, with an array of data. 
 
-// check if queryField exists in data object, if not, create it. 
-// check if fieldName exists as a key within data.queryField, if not create it and initialize it to empty array. 
-// push the obj containing {speed, id} into the array.
 const updateParams = {
   TableName: "UserData",
   Key: { UserID: "testData"},
@@ -93,15 +90,61 @@ const updateParams = {
   }
 }
 
-let params1 = {
+/*
+Step 1: Check if UserID exists in DB. 
+Step 2: Check if QueryField Exists as a key within User's Data Object | 
+if it does not exist, add it onto the existing Data object and initialize it (to an array with 1 element)
+Step 3: If it does exist, do nothing and move onto step 4. 
+Step 4: Add a new QueryName to data if it does not yet exist. If exists, go to step 5
+Step 5: Append to existing object. 
+
+*/
+
+
+// Params Object to add a new field key to an existing data Oject -- does not work when the queryName has not yet been added. 
+const newFieldParams = {
+  TableName: "UserData",
+  Key: { UserID : "testData"},
+  ExpressionAttributeNames: {
+    "#d": "Data",
+    "#t": "State", // QueryField
+    "#f": "total_dosage" // FieldName 
+  },
+  ExpressionAttributeValues: {
+    ':v' : [{  "speed": 0.4944, // speed
+    "frequency": 1,
+    "time": 1567113437211, // time 
+    "id": "1ls2jzx6vmzf"}]
+  },
+  UpdateExpression : 'Set #d.#t.#f = :v',
+  ConditionExpression: "attribute_not_exists(#d.#t.#f)"
+}
+
+// Params object to add a new QueryName to data if it does not yet exist. -- (should happen before the above param ^ )
+const newCustomTypeParam = {
+  TableName: "UserData",
+  Key: {UserID:"testData"},
+  ExpressionAttributeNames: {
+    "#d": "Data",
+    "#t" : "State",
+  },
+  ExpressionAttributeValues: {
+    ':v' : {}
+  },
+  UpdateExpression: "Set #d.#t = :v",
+  ConditionExpression: "attribute_not_exists(#d.#t)"
+}
+
+
+const existingFieldParams = {
   TableName: "UserData",
   ExpressionAttributeNames: {
       "#Y": "Data",
-      '#Z': 'Query',
-      '#A' : 'stte'
+      '#Z': 'State',
+      '#A': 'total_dosage'
   },
   ExpressionAttributeValues: {
-      ":y": [{hi:"PersonXYZ",lol: 'Bitchass'}]
+      ":y": [{hi:"PersonXYZ",lol: 'Bitchass', andrew: "TAAANGG"}] // resolver data object. 
   },
   Key: {
       UserID: 'testData'
@@ -109,44 +152,8 @@ let params1 = {
   UpdateExpression: "SET #Y.#Z.#A = list_append(#Y.#Z.#A,:y)"
 };
 
-let params2 = {
-  TableName: "UserData",
-  ExpressionAttributeNames: {
-    '#Y' : "Data",
-    "#Z" : "State",
-    "#A" : "name"
-  },
-  ExpressionAttributeValues: {
-    ":y" : [{god: "Andrew"}]
-  },
-  Key: {
-    UserID: 'testData'
-  },
-  UpdateExpression: "SET #Y.#Z.#A = :y"
-}
+db.update(existingFieldParams,(err,data) => {
+  if (err) console.log(err);
+  else console.log(data);
+})
 
-
-
-
-
-db.update(params1).promise().then((data)=>{
-console.log('successfuly did it');
-console.log(data);
-}), err => {
-  db.update(params2).promise().then((data)=>{
-    console.log('initialized data')
-  })
-  .catch((e)=>{
-    console.log(e)
-  })
-}
-
-// db.update(params1, (err,data) => {
-//   if (err) console.log(err, err.stack);
-//   else console.log('success')
-// })
-
-
-
-
-module.exports = db
