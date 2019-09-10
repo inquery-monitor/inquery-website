e43cef8c99dc4f1c87b3cff74e438582e59d0d9d
Andrew Tang <atreeleaf@gmail.com>
2019-09-10 00:44:43 -0700

    m

--- client/components/BodyElements/Home.tsx
+++ client/components/BodyElements/Home.tsx
@@ -5,7 +5,7 @@ function Home(){
   return (
     <div className='main-body'>
   <img src = 'https://raw.githubusercontent.com/rohan-varma/rohan-blog/gh-pages/images/graphql.png' id = 'graph-ql'></img>
-  <h3 id = 'main-description'>GoblinQL Monitor Asshole</h3>
+  <h3 id = 'main-description'>GoblinQL Monitor</h3>
   <h3 id = 'sub-description'>Monitor your GraphQL endpoint.</h3>
   <div id = 'action-buttons-wrapper'>
     <button className = 'action-btns'> Get Started </button>
