91704034c2c7bc955a80ace6794a990cfa074133
justin fung <justinfung@justins-MacBook-Pro.local>
2019-09-10 20:33:03 -0700

    update styles: css and copy
    update styles: css and copy

--- client/components/BodyElements/Community.tsx
+++ client/components/BodyElements/Community.tsx
@@ -2,9 +2,32 @@ import React from 'react';
 
 
 function Community(){
-  return(
+  // init an object to hold our community info.
+  const communityInfo: Array<object> = [
+    {
+      'title': 'GitHub',
+      'textHTML': 'GoblinQL Monitor is in active development.  Check out <a class=\'link\' href=\'https://github.com/goblin-monitoring-graphql/\'>the repository</a> at GitHub.com.'
+    }
+  ];
+
+  // Build an array of JSX components from the data.
+  const communityElements = communityInfo.map((community: object, idx: number) => {
+    return (
+      <div className='community' id={'community-'+idx} key={idx}>
+        <div className='community-title'>{community['title']}</div>
+        <div className='community-text'
+             dangerouslySetInnerHTML={{ __html: community['textHTML']}}>
+        </div>
+      </div>
+    )
+  })
+
+  // Community component renders the following.
+  return (
     <div className='community-body'>
-    Community
+      <div className='community-wrapper'>
+        {communityElements}
+      </div>
     </div>
   )
 }
