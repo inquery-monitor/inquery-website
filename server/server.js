f41468e4c702b620ea321467a318d86b9beb9cf9
Andrew Tang <atreeleaf@gmail.com>
2019-09-11 17:34:13 -0700

    merge conflicts resolved

--- server/server.js
+++ server/server.js
@@ -11,12 +11,14 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(authMiddleware.checkJwt);
 
-<<<<<<< HEAD
-app.use(express.static(path.join(__dirname,'../client')));
 
-app.get('/', (req, res) => {
+// Serve static assets.
+app.use(express.static(path.join(__dirname,'../client')))
+
+// Serve homepage.
+app.get('/',(req, res) => {
  res.sendFile(path.join(__dirname,'../client/index.html'))
-});
+})
 
 app.get('/requestApiKey', apiMiddleware.generateAuthKeys, dynamoController.createUser, (req, res) => {
   res.status(200).json(res.locals.authKeys);
@@ -26,39 +28,13 @@ app.post('/checkApiKey', dynamoController.checkApiKey, authMiddleware.setJwt, (r
   res.status(301).send()
 })
 
-
-
-app.get('/analytics', (req,res) => {
-  console.log('analytics hit.')
-=======
-// Serve static assets.
-app.use(express.static(path.join(__dirname,'../client')))
-
-// Serve homepage.
-app.get('/',(req, res) => {
- res.sendFile(path.join(__dirname,'../client/index.html'))
-})
-
 // Serve analytics.
 app.get('/analytics', (req, res) => {
->>>>>>> 28296c6ac78a9bc9bebe4358707416a3f97ed5be
   res.sendFile(path.join(__dirname,'../client/dashboard/index.html'))
 })
 
 // Handle data requests.
 app.use('/data', dbRouter);
 
-<<<<<<< HEAD
-
-app.use('/data', dbRouter);
-
-
-
-
-app.listen(3000, () => {
-  console.log('listening on port 3000')
-});
-=======
 // I'm listening...
 app.listen(3000, () => console.log('listening on port 3000'));
->>>>>>> 28296c6ac78a9bc9bebe4358707416a3f97ed5be
